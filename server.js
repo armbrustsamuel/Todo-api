a0d6fb82bd6e88e1220a213e57d11c02e38b47d9
Samuel Armbrust <Samuel@MacBook-Pro-do-Samuel.local>
2016-11-18 22:01:30 -0200

    /todos/:id

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3001;
+var todos = [{
+	id: 1,
+	description: 'Meet mom for lunch',
+	completed: false
+},{
+	id: 2,
+	description: 'Go to market',
+	completed: false
+},{
+	id: 3,
+	description: 'Do homework',
+	completed: false
+}];
 
 app.get('/', function (req,res) {
 	res.send('Todo API root');
+});
+
+// GET /todos
+app.get('/todos', function(req,res){
+	res.json(todos);
+});
+
+// GET /todos/:id
+app.get('/todos/:id', function(req,res){
+	res.send('Asking for todo with id of ' + req.params.id )
 });
 
 app.listen(PORT, function(){
