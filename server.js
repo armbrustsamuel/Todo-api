d7ab2baee26b195cfe1b81495c17599c2a1bfce3
Samuel Armbrust <Samuel@MacBook-Pro-do-Samuel.local>
2017-05-08 13:12:36 -0300

    Convert GET /todos to use sequelize

--- server.js
+++ server.js
@@ -16,30 +16,49 @@ app.get('/', function(req, res) {
 
 // GET /todos
 app.get('/todos', function(req, res) {
-	var queryParams = req.query;
-	var filteredTodos = todos;
+	var query = req.query;
+	var where = {};
 
-	// if has property && compelted === 'true'
-	// filteredTodos = _.where(filteredtodos, ?)
-	// else if has prop && completed if 'false'
-
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: true
-		});
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: false
-		});
+	if(query.hasOwnProperty('completed') && query.completed == "true") {
+		where.completed = true;
+	} else if (query.hasOwnProperty('completed') && query.completed == "false") {
+		where.completed = false;
 	}
 
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos = _.filter(filteredTodos, function(todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.toLowerCase()) > -1
-		});
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = {
+			$like: '%' + query.q + '%'
+		};
 	}
 
-	res.json(filteredTodos);
+	db.todo.findAll({where: where}).then (function(todos){
+		res.json(todos);
+	}, function(e){
+		res.status(500).send();
+	});
+	// var filteredTodos = todos;
+
+	// // if has property && compelted === 'true'
+	// // filteredTodos = _.where(filteredtodos, ?)
+	// // else if has prop && completed if 'false'
+
+	// if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+	// 	filteredTodos = _.where(filteredTodos, {
+	// 		completed: true
+	// 	});
+	// } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+	// 	filteredTodos = _.where(filteredTodos, {
+	// 		completed: false
+	// 	});
+	// }
+
+	// if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+	// 	filteredTodos = _.filter(filteredTodos, function(todo) {
+	// 		return todo.description.toLowerCase().indexOf(queryParams.toLowerCase()) > -1
+	// 	});
+	// }
+
+	// res.json(filteredTodos);
 });
 
 // GET /todos/:id
