1c2bea0f0443e4171cf308c72da4dab9d64549fa
Samuel Armbrust <Samuel@MacBook-Pro-do-Samuel.local>
2017-05-08 19:12:01 -0300

    Convert DELETE /todos/:id to sequelize

--- server.js
+++ server.js
@@ -124,19 +124,49 @@ app.post('/todos', function(req, res) {
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
+
+	// // TEST THAT WORKED
+	// db.todo.findById(todoId).then (function (todo) {
+	// 	if(!!todo) {
+	// 		res.json(todo.toJSON());
+	// 		todo.destroy();
+	// 	} else {
+	// 		res.status(400).send();
+	// 	}
+	// }, function(e) {
+	// 	res.status(500).send();
+	// });
+
+	db.todo.destroy({
+		where :{
+			id:todoId
+		}
+	}).then(function(rowsDeleted) {
+		if(rowsDeleted == 0){
+			res.status(404).json({
+				error: 'No todo with id'
+			});
+		} else {
+			res.status(204).send();
+		}
+	}, function () {
+		res.status(500).send();
 	});
 
-	if (!matchedTodo) {
-		res.status(404).json({
-			"error": "no todo found with that id"
-		});
-	} else {
-		todos = _.without(todos, matchedTodo);
-	}
 
-	res.json(matchedTodo);
+	// var matchedTodo = _.findWhere(todos, {
+	// 	id: todoId
+	// });
+
+	// if (!matchedTodo) {
+	// 	res.status(404).json({
+	// 		"error": "no todo found with that id"
+	// 	});
+	// } else {
+	// 	todos = _.without(todos, matchedTodo);
+	// }
+
+	// res.json(matchedTodo);
 });
 
 // PUT /todos/:id
