887b56875c73fe52475311e895537a70be22f789
Samuel Armbrust <Samuel@MacBook-Pro-do-Samuel.local>
2017-10-22 21:31:11 -0200

    Hash and salt password

--- server.js
+++ server.js
@@ -217,11 +217,12 @@ app.put('/todos/:id', function(req, res) {
 
 });
 
+// POST /users
 app.post('/users', function (req,res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.create(body).then(function(user){
-		res.json(user.toJSON());
+		res.json(user.toPublicJSON());
 	}, function (e) {
 		res.status(400).json(e);
 	});
